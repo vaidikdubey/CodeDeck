d7c765c876b55865564f8a52851ef43633538dba
vaidikdubey <vaidik26dubey@gmail.com>
2025-04-24 18:22:31 +0530

    feat: Setup authentication routes and controllers with auth middleware to verify user login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config()
@@ -9,6 +12,8 @@ const app = express()
 const port = process.env.PORT ?? 8080
 
 app.use(express.json())
+app.use(cookieParser())
+
 
 app.get("/", (req, res) => {
     res.send("Hello there, Welcome to CodeDeck🔥")
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { check, login, logout, register } from "../controllers/auth.controllers.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const authRoutes = express.Router();
 
@@ -8,8 +9,8 @@ authRoutes.post("/register", register)
 
 authRoutes.post("/login", login)
 
-authRoutes.post("/logout", logout)
+authRoutes.post("/logout", authMiddleware, logout)
 
-authRoutes.get("/check", check)
+authRoutes.get("/check", authMiddleware, check)
 
 export default authRoutes
