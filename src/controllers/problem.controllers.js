2636204b717967bcb51bbb01ad764106a173ea2b
vaidikdubey <vaidik26dubey@gmail.com>
2025-05-06 22:33:03 +0530

    Created createProblem controller with judge0-language-id, submission-batch and poll-batch-results libs under judge0.libs.js

--- src/controllers/problem.controllers.js
+++ src/controllers/problem.controllers.js
@@ -1,4 +1,5 @@
 import { db } from "../libs/db.js";
+import { getJudge0LanguageId, pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
 
 export const createProblem = async (req, res) => {
     // 1 - Get all the data from request body
@@ -32,9 +33,55 @@ export const createProblem = async (req, res) => {
                     error: `Language ${language} is not supported`
                 })
             }
+
+            const submissions = testcases.map(({ input, output }) => ({
+                source_code: solutionCode,
+                language_id: languageId,
+                stdin: input,
+                expected_output: output,
+            }))
+
+            const submissionResults = await submitBatch(submissions);
+
+            //Extracting tokens from submissions returned by Judge0
+            const tokens = submissionResults.map((res) => res.token);
+
+            //Polling results from Judge0
+            const results = await pollBatchResults(token);
+
+            for (let i = 0; i < results.length; i++) {
+                const result = results[i];
+
+                if (result.status.id !== 3) {
+                    return res.status(400).json({
+                        error: `Testcase ${i+1} failed for language ${language}`
+                    })
+                }
+            }
+
+            //Step 4
+            const newProblem = await db.problem.create({
+                data: {
+                    title,
+                    description,
+                    difficulty,
+                    tags,
+                    examples,
+                    constraints,
+                    testcases,
+                    codeSnippet,
+                    referenceSolutions,
+                    userId: req.user.id,
+                }
+            })
+
+            return res.status(201).json(newProblem);
         }
     } catch (error) {
-        
+        console.error("Error creating problem: ", error);
+        return res.status(500).json({
+            error: "Error creating problem"
+        })
     }
 };
 
--- src/libs/judge0.lib.js
+++ src/libs/judge0.lib.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const getJudge0LanguageId = (language) => {
     const languageMap = {
         "PYTHON": 71,
@@ -6,4 +8,38 @@ export const getJudge0LanguageId = (language) => {
     }
 
     return languageMap[language.toUpperCase()]
+}
+
+const sleep = (ms) => new Promise ((resolve) => setTimeout(resolve, ms))
+
+export const pollBatchResults = async (tokens) => {
+    while (true) {
+        const { data } = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`, {
+            params: {
+                tokens: tokens.join(","),
+                base64_encoded:false,
+            }
+        })
+
+        const results = data.submissions;
+
+        const isAllDone = results.every(
+            (result) => result.status_id !== 1 && result.status_id !== 2
+        )
+
+        if (isAllDone) return results;
+
+        //Waiting 1s before hitting the end point again
+        await sleep(1000);
+    }
+}
+
+export const submitBatch = async (submissions) => {
+    const { data } = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`, {
+        submissions
+    })
+
+    console.log("Submissions Results: ", data);
+
+    return data; //[{token}, {token}, {token}]
 }
