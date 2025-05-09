36c688025afe75769f7311976d225502227490b2
vaidikdubey <vaidik26dubey@gmail.com>
2025-05-09 23:52:45 +0530

    Implemented fully working and robust execute code controller with getLanguageIdByName utility

--- src/libs/judge0.lib.js
+++ src/libs/judge0.lib.js
@@ -43,3 +43,14 @@ export const submitBatch = async (submissions) => {
 
     return data; //[{token}, {token}, {token}]
 }
+
+export function getLanguageName(languageId) {
+    const LANGUAGE_NAMES = {
+        74: "TypeScript",
+        63: "JavaScript",
+        71: "Python",
+        62: "Java",
+    }
+
+    return LANGUAGE_NAMES[languageId] || "Unknown";
+}
