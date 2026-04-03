3399c4fcb9b0062bc7597cb3171825c510079cd2
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-03 19:01:41 +0530

    Updated axios instance util with deployed backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
-    withCredentials: true
-})
+    baseURL:
+        import.meta.env.MODE === "development"
+            ? "http://localhost:8080/api/v1"
+            : import.meta.env.VITE_API_URL,
+    withCredentials: true,
+});
