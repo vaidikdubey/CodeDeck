a4add334728dcdb65b5f7886cdf0983c1e7d6c74
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-03 19:11:00 +0530

    Updated backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
     baseURL:
         import.meta.env.MODE === "development"
             ? "http://localhost:8080/api/v1"
-            : import.meta.env.VITE_API_URL,
+            : `${import.meta.env.VITE_API_URL}/api/v1`,
     withCredentials: true,
 });
